377960d6e0ea5638135432ac900b725c71d9faf6
Harshv1209 <1320204019@bitraipur.ac.in>
2024-06-02 15:31:26 +0530

    added callpage

--- src/pages/dashboard/Call.js
+++ src/pages/dashboard/Call.js
@@ -1,7 +1,99 @@
-import React from "react"
+import {
+  Box,
+  Divider,
+  IconButton,
+  Link,
+  Stack,
+  Typography,
+} from "@mui/material"
+import React, { useState } from "react"
+import {
+  Search,
+  SearchIconWrapper,
+  StyledInputBase,
+} from "../../components/Search"
+import { MagnifyingGlass, Plus } from "phosphor-react"
+import { useTheme } from "@mui/material/styles"
+import { SimpleBarStyle } from "../../components/Scrollbar"
+import { CallLogs } from "../../data"
+
+import { CallLogElement } from "../../components/CallElement"
+import StartCall from "../../sections/main/StartCall"
 
 const Call = () => {
-  return <div>Call</div>
+  const theme = useTheme()
+  const [openDialog, setOpenDialog] = useState(false)
+  const handleCloseDialog = () => {
+    setOpenDialog(false)
+  }
+
+  return (
+    <>
+      <Stack direction={"row"} sx={{ width: "100%" }}>
+        {/* Left */}
+        <Box
+          sx={{
+            height: "100vh",
+            backgroundColor: (theme) =>
+              theme.palette.mode === "light"
+                ? "#F8FAFF"
+                : theme.palette.background,
+            width: 320,
+            boxShadow: "0px 0px 2px rgba(0,0,0,0.25)",
+          }}
+        >
+          <Stack p={3} spacing={2} sx={{ maxHeight: "100vh" }}>
+            <Stack>
+              <Typography variant="h5">Call Logs</Typography>
+            </Stack>
+            <Stack sx={{ width: "100%" }}>
+              <Search>
+                <SearchIconWrapper>
+                  <MagnifyingGlass color="#709CE6" />
+                </SearchIconWrapper>
+                <StyledInputBase placeholder="Search..." />
+              </Search>
+            </Stack>
+            <Stack
+              direction={"row"}
+              justifyContent={"space-between"}
+              alignItems={"center"}
+            >
+              <Typography variant="subtitle2" component={Link}>
+                Start Conversation
+              </Typography>
+              <IconButton
+                onClick={() => {
+                  setOpenDialog(true)
+                }}
+              >
+                <Plus style={{ color: theme.palette.primary.main }} />
+              </IconButton>
+            </Stack>
+            <Divider />
+            <Stack
+              sx={{ flexGrow: 1, overflowY: "scroll", height: "100%" }}
+              spacing={3}
+            >
+              <SimpleBarStyle timeout={500} clickOnTrack={false}>
+                <Stack spacing={2.5}>
+                  {/* Call Log */}
+                  {CallLogs.map((el) => (
+                    <CallLogElement {...el} />
+                  ))}
+                </Stack>
+              </SimpleBarStyle>
+            </Stack>
+          </Stack>
+        </Box>
+
+        {/* Right */}
+      </Stack>
+      {openDialog && (
+        <StartCall open={openDialog} handleClose={handleCloseDialog} />
+      )}
+    </>
+  )
 }
 
 export default Call
